51c1420b4afbd8ce0514fdbb0585546faefeefe4
Roman <roman.kushka10@gmail.com>
2020-12-12 18:50:41 +0200

    insert Angular syntax (loop) in header component

---
+++ src/app/header/header.constants.ts
@@ -0,0 +1,33 @@
+export const NAVMENU = [
+    {
+        title: 'Home',
+        link: ''
+    },
+    {
+        title: 'About',
+        link: '/about'
+    },
+    {
+        title: 'Shop',
+        link: '/shop'
+    },
+    {
+        title: 'Contacts',
+        link: '/contact'
+    }
+]
+
+export const ItemsHeader = [
+    {
+        title: 'customer support'
+    },
+    {
+        title: 'shipping'
+    },
+    {
+        title: 'login'
+    },
+    {
+        title: 'info'
+    }
+]
--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { WATCHES } from './shop.constants';
 
 @Component({
   selector: 'app-shop',
@@ -6,55 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shop.component.scss']
 })
 export class ShopComponent implements OnInit {
-
-  watches = [
-    {
-      name: 'Watch 1',
-      description: 'Description 1',
-      price: 1
-    },
-    {
-      name: 'Watch 2',
-      description: 'Description 2',
-      price: 2
-    },
-    {
-      name: 'Watch 3',
-      description: 'Description 3',
-      price: 3
-    },
-    {
-      name: 'Watch 4',
-      description: 'Description 4',
-      price: 4
-    },
-    {
-      name: 'Watch 5',
-      description: 'Description 5',
-      price: 5
-    },
-    {
-      name: 'Watch 6',
-      description: 'Description 6',
-      price: 6
-    },
-    {
-      name: 'Watch 7',
-      description: 'Description 7',
-      price: 7
-    },
-    {
-      name: 'Watch 8',
-      description: 'Description 8',
-      price: 8
-    },
-    {
-      name: 'Watch 9',
-      description: 'Description 9',
-      price: 9
-    }
-  ];
-
+  watches = WATCHES;
+  
   constructor() { }
 
   ngOnInit(): void {
