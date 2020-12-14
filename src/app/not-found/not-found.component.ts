32553d91b22fe51d6f02bfb3f1e456cf84053013
Roman <roman.kushka10@gmail.com>
2020-12-14 18:29:09 +0200

    finished not found page

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DESCRIPTION, LINKS, PHOTOS } from './not-found.constants';
 
 @Component({
   selector: 'app-not-found',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./not-found.component.scss']
 })
 export class NotFoundComponent implements OnInit {
+  description = DESCRIPTION;
+  photos = PHOTOS
+  links = LINKS;
 
   constructor() { }
 
---
+++ src/app/not-found/not-found.constants.ts
@@ -0,0 +1,18 @@
+export const DESCRIPTION = [
+    {
+        title: 'Not found'
+    }
+]
+
+export const PHOTOS = [
+    {
+        path: '../../assets/image/sad.svg'
+    }
+]
+
+export const LINKS = [
+    {
+        title: 'Home',
+        link: ''
+    },
+]
