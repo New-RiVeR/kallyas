cfe12de315b8f6bef00cbb8aeabd0392d3628394
RomanTseduliak <alligarch@gmail.com>
2020-12-21 15:11:57 +0200

    add home page

---
+++ src/app/home/home.constants.ts
@@ -0,0 +1,53 @@
+export const HOME_CONTENT_BLOCK = [
+  
+  {
+      id: 1,
+      button: 'MORE',
+      description: 'LUXURY WATCHES',
+      paragraph: 'Discover our finely curated collection of investment-worthy diamonds and gemstones, breathtaking designs, and exceptional craftsmanship',
+      photo_path: '../../assets/image/about-clock-1.jpg',
+      orderImg: 1
+  },
+  {   id:2,
+      button: 'MORE',
+      description: 'SPORTS WATCHES',
+      paragraph: 'Discover our finely curated collection of investment-worthy diamonds and gemstones, breathtaking designs, and exceptional craftsmanship',
+      photo_path: '../../assets/image/about-clock-2.jpg',
+      orderImg: 2
+  }
+  
+];
+export const WATCHES = [
+  {
+    id: 1,
+    name: 'Watch 1',
+    description: 'Description 1',
+    price: 1,
+    image: '../../assets/image/clock-1.jpg',
+    btnActive: false
+  },
+  {
+    id: 2,
+    name: 'Watch 2',
+    description: 'Description 2',
+    price: 2,
+    image: '../../assets/image/clock-2.jpg',
+    btnActive: false
+  },
+  {
+    id: 3,
+    name: 'Watch 3',
+    description: 'Description 3',
+    price: 3,
+    image: '../../assets/image/clock-3.jpg',
+    btnActive: false
+  },
+  {
+    id: 4,
+    name: 'Watch 4',
+    description: 'Description 4',
+    price: 4,
+    image: '../../assets/image/clock-4.jpg',
+    btnActive: false
+  },
+];
