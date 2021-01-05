ec163e4b1bc9baf483974a9849a40404677b6fab
Roman <roman.kushka10@gmail.com>
2021-01-05 16:21:58 +0200

    create new func only on admin shop

---
+++ src/app/services/watch.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { WatchItem } from '../models/IWatch';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WatchService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  getWatches():Observable<WatchItem[]>{
+    return this.httpClient.get<WatchItem[]>('http://localhost:3000/watches');
+  }
+
+  removeWatch(id):Observable<{}>{
+    return this.httpClient.delete(`http://localhost:3000/watches/${id}`)
+  }
+
+  addWatch(formValue):Observable<WatchItem>{
+    const watch = formValue
+    return this.httpClient.post<WatchItem>('http://localhost:3000/watches', watch);
+  }
+
+}
